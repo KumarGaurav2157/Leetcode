c6df7220c18e2471d3ebf92deb9ee7ffb3d23398
Kumar Gaurav <kumargaurav81277@gmail.com>
2026-02-05 19:48:34 +0530

    Time: 13 ms (5.85%), Space: 26 MB (8.08%) - LeetHub

--- 3379-transformed-array/3379-transformed-array.cpp
+++ 3379-transformed-array/3379-transformed-array.cpp
@@ -1,28 +1,16 @@
 class Solution {
 public:
     vector<int> constructTransformedArray(vector<int>& nums) {
-       vector<int>ans;
-       int n=nums.size();
-       for(int i=0;i<nums.size();i++)
-       {
-          
-           if(nums[i]>0)
-           {
-            ans.push_back(nums[(i+nums[i])%n]);
-
-           }
-           else if(nums[i]<0)
-           {
-             int x=abs(nums[i])%n;
-             if((i-x)>=0)
-             ans.push_back(nums[i-x]);
-             else
-             ans.push_back(nums[n-(x-i)]);
-           }
-           else
-           ans.push_back(nums[i]);
-       }
-       return ans;
+      vector<int>ans;
+      int n=nums.size();
+      for(int i=0;i<n;i++)
+      {
+        int x=(i+nums[i])%n;
+        if(x<0)
+        x+=n;
+        ans.push_back(nums[x]);
+      }
+      return ans;
 
            
        }
